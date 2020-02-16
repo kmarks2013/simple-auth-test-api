1cd99e83e8619626d7c520f2b55d907afa871b5a
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:50:57 -0500

    adds private method user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
     def create
         user = User.create()
     end
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
 end
