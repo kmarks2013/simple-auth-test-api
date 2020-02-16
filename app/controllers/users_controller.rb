7641b31e18c409e9cfe8d63ad5d7c0b580f1e8b4
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:55:48 -0500

    fixes valid method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def create
         user = User.create(user_params)
-        if user.is_valid?
+        if user.valid?
             render json: user
         else
             render json: {errors: user.errors.full_messages}
