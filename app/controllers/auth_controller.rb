cfa776f1ccf82b10f10c6414ae3d03187936f4b5
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:42:12 -0500

    fixes params spelling error

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,6 @@
 class AuthController < ApplicationController
     def login
-        user = User.find_by(username: param[:username])
+        user = User.find_by(username: params[:username])
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
             render json: user
