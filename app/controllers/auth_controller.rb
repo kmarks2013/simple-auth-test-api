d0bcc17bba7cfad0d5621e3d17ab9f55c26ce313
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:40:38 -0500

    checks if authentication is truthy and renders JSON of use

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,5 +2,8 @@ class AuthController < ApplicationController
     def login
         user = User.find_by(username: param[:username])
         is_authenticated = user.authenticate(params[:password])
+        if is_authenticated
+            render json: user
+        end
     end
 end
