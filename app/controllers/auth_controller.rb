a0ba23a4cb046a2b351fc7be5d834ad63b8419ce
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:39:05 -0500

    adds authenticate method and save sit to a variable

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,6 @@
 class AuthController < ApplicationController
     def login
         user = User.find_by(username: param[:username])
+        is_authenticated = user.authenticate(params[:password])
     end
 end
