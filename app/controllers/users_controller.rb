29b6bb5faa51cc198a4c18f1c7fb0f5d99b1d67b
kmarks2013 <kmarks2013@gmail.com>
2020-02-16 16:52:30 -0500

    updates create method with user params and checks if a user is valid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
     def create
-        user = User.create()
+        user = User.create(user_params)
+        if user.is_valid?
+            render json: user
+        else
+            render json: {errors: user.errors.full_messages}
+        end
     end
     private
     def user_params
