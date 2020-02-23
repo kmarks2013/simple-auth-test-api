0e5eef666ad8117f4ca2f2397fc9d53b0437a09d
kmarks2013 <kmarks2013@gmail.com>
2020-02-23 13:02:34 -0500

    adds password complexity regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,13 @@ class User < ApplicationRecord
     # password validations
     has_secure_password
     validates :password, :length => { :minimum => 6 }
+    validate :password_complexity
 
+    def password_complexity
+        return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+        errors.add :password, 'Password must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    end
+    
 
 end
