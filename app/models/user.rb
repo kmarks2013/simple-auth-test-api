56d207a6dee3b9bb26d57ccc83c2d945308f8d2c
kmarks2013 <kmarks2013@gmail.com>
2020-02-23 13:16:56 -0500

    adds space in username lenght validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     # user validations
     validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
-    validates :username, :length => { :in => 6..30}
+    validates :username, :length => { :in => 6..30 }
     
     # password validations
     has_secure_password
