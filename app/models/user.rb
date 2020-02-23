522c2a761c961307963b3548783627bbe9b3dfdb
kmarks2013 <kmarks2013@gmail.com>
2020-02-23 13:17:35 -0500

    adds password format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-    
+    PASSWORD_FORMAT = /\A
+        (?=.*\d)           # Must contain a digit
+        (?=.*[a-z])        # Must contain a lower case character
+        (?=.*[A-Z])        # Must contain an upper case character
+        (?=.*[[:^alnum:]]) # Must contain a symbol
+    /x
     # user validations
     validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
