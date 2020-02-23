65b0e7502c40d729ec1d17e97ecb6a87b9071478
kmarks2013 <kmarks2013@gmail.com>
2020-02-23 12:37:05 -0500

    adds length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
     # user validations
     validates_presence_of :username
     validates_uniqueness_of :username, :case_sensitive => false
+    validates :username, :length => { :in => 6..30}
 
 end
