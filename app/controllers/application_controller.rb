a6b13b8c9486ae03154a2b77dbc12b8c0c10dffe
kmarks2013 <kmarks2013@gmail.com>
2020-03-01 14:05:44 -0500

    updates token in login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    def encode_token(payload)
+        JWT.encode(payload, hmac_secret, 'HS256')
+    end
+
+    def user_payload(user)
+        {user_id: user.id}
+    end
+
+    def hmac_secret
+        ENV['JWT_SECRET_KEY']
+    end
+
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,9 +3,7 @@ class AuthController < ApplicationController
         user = User.find_by(username: params[:username])
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
-            payload = {user_id: user.id}
-            token = JWT.encode(payload, "examplesecretkey", 'HS256')
-            render json: {token: token}
+            render json: {token: encode_token(user_payload(user))}
         else
             render json: {error: "Wrong username/password. Please try again!"}
         end
