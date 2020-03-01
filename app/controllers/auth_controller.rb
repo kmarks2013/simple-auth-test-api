094c8e26374d9a8564b5da5072409ad0e5c5fcc2
kmarks2013 <kmarks2013@gmail.com>
2020-03-01 13:11:58 -0500

    adds jwt token creation to auth contorller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,7 +3,9 @@ class AuthController < ApplicationController
         user = User.find_by(username: params[:username])
         is_authenticated = user.authenticate(params[:password])
         if is_authenticated
-            render json: user
+            payload = {user_id: user.id}
+            token = JWT.encode(payload, "examplesecretkey", 'HS256')
+            render json: {token: token}
         else
             render json: {error: "Wrong username/password. Please try again!"}
         end
